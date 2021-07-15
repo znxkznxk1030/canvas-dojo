0158529b2b0f999e1da8e86e129e77712b9cbf80
ysKim <znxkznxk1030@gmail.com>
2021-07-16 00:00:46 +0900

    Webview | connect to active editor

---
+++ src/CanvasPanel.ts
@@ -0,0 +1,169 @@
+import * as vscode from "vscode"
+
+class CanvasPanel {
+	/**
+	 * Track the currently panel. Only allow a single panel to exist at a time.
+	 */
+	public static currentPanel: CanvasPanel | undefined;
+
+	public static readonly viewType = 'canvas-dojo.canvasView';
+
+	private readonly _panel: vscode.WebviewPanel;
+	private readonly _extensionUri: vscode.Uri;
+	private _disposables: vscode.Disposable[] = [];
+
+	public static createOrShow(extensionUri: vscode.Uri) {
+		const column = vscode.window.activeTextEditor
+			? vscode.window.activeTextEditor.viewColumn
+			: undefined;
+
+		// If we already have a panel, show it.
+		if (CanvasPanel.currentPanel) {
+			CanvasPanel.currentPanel._panel.reveal(column);
+			return;
+		}
+
+		// Otherwise, create a new panel.
+		const panel = vscode.window.createWebviewPanel(
+			CanvasPanel.viewType,
+			'Canvas Preview',
+			column || vscode.ViewColumn.One,
+			getWebviewOptions(extensionUri),
+		);
+
+		CanvasPanel.currentPanel = new CanvasPanel(panel, extensionUri);
+	}
+
+	public static revive(panel: vscode.WebviewPanel, extensionUri: vscode.Uri) {
+		CanvasPanel.currentPanel = new CanvasPanel(panel, extensionUri);
+	}
+
+	private constructor(panel: vscode.WebviewPanel, extensionUri: vscode.Uri) {
+		this._panel = panel;
+		this._extensionUri = extensionUri;
+
+		// Set the webview's initial html content
+		this._update();
+
+		// Listen for when the panel is disposed
+		// This happens when the user closes the panel or when the panel is closed programmatically
+		this._panel.onDidDispose(() => this.dispose(), null, this._disposables);
+
+		// Update the content based on view changes
+		this._panel.onDidChangeViewState(
+			e => {
+				if (this._panel.visible) {
+					this._update();
+				}
+			},
+			null,
+			this._disposables
+		);
+
+		// Handle messages from the webview
+		this._panel.webview.onDidReceiveMessage(
+			message => {
+				switch (message.command) {
+					case 'alert':
+						vscode.window.showErrorMessage(message.text);
+						return;
+				}
+			},
+			null,
+			this._disposables
+		);
+	}
+
+	public doRefactor() {
+		// Send a message to the webview webview.
+		// You can send any JSON serializable data.
+		this._panel.webview.postMessage({ command: 'refactor' });
+	}
+
+	public dispose() {
+		CanvasPanel.currentPanel = undefined;
+
+		// Clean up our resources
+		this._panel.dispose();
+
+		while (this._disposables.length) {
+			const x = this._disposables.pop();
+			if (x) {
+				x.dispose();
+			}
+		}
+	}
+
+	private _update() {
+		const webview = this._panel.webview;
+		this._panel.webview.html = this._getHtmlForWebview(webview);
+	}
+
+	private _getHtmlForWebview(webview: vscode.Webview) {
+		// Get the local path to main script run in the webview, then convert it to a uri we can use in the webview.
+		// const scriptUri = webview.asWebviewUri(vscode.Uri.joinPath(this._extensionUri, 'media', 'main.js'));
+
+    const editor = vscode.window.activeTextEditor;
+    let scriptCode = "";
+    if (editor) {
+      const document = editor.document;
+      scriptCode = document.getText();
+      console.log(scriptCode);
+    }
+
+		// Do the same for the stylesheet.
+		const styleResetUri = webview.asWebviewUri(vscode.Uri.joinPath(this._extensionUri, 'media', 'reset.css'));
+		const styleVSCodeUri = webview.asWebviewUri(vscode.Uri.joinPath(this._extensionUri, 'media', 'vscode.css'));
+		const styleMainUri = webview.asWebviewUri(vscode.Uri.joinPath(this._extensionUri, 'media', 'main.css'));
+
+		// Use a nonce to only allow a specific script to be run.
+		const nonce = getNonce();
+
+		return `<!DOCTYPE html>
+			<html lang="en">
+			<head>
+				<meta charset="UTF-8">
+				<!--
+					Use a content security policy to only allow loading images from https or from our extension directory,
+					and only allow scripts that have a specific nonce.
+				-->
+				<meta http-equiv="Content-Security-Policy" content="default-src 'none'; style-src ${webview.cspSource}; script-src 'nonce-${nonce}';">
+				<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+				<link href="${styleResetUri}" rel="stylesheet">
+				<link href="${styleVSCodeUri}" rel="stylesheet">
+				<link href="${styleMainUri}" rel="stylesheet">
+				
+				<title>Cat Colors</title>
+			</head>
+			<body>
+        <canvas></canvas>
+				<script nonce="${nonce}">
+        ${scriptCode}
+        </script>
+			</body>
+			</html>`;
+	}
+}
+
+function getNonce() {
+	let text = '';
+	const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+	for (let i = 0; i < 32; i++) {
+		text += possible.charAt(Math.floor(Math.random() * possible.length));
+	}
+	return text;
+}
+
+function getWebviewOptions(extensionUri: vscode.Uri): vscode.WebviewOptions {
+	return {
+		// Enable javascript in the webview
+		enableScripts: true,
+
+		// And restrict the webview to only loading content from our extension's `media` directory.
+		localResourceRoots: [vscode.Uri.joinPath(extensionUri, 'media')]
+	};
+}
+
+
+export default CanvasPanel;
--- src/extension.ts
+++ src/extension.ts
@@ -1,100 +1,117 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
-
+import CanvasPanel from './CanvasPanel';
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+  context.subscriptions.push(
+		vscode.commands.registerCommand('canvas-dojo.start', () => {
+			CanvasPanel.createOrShow(context.extensionUri);
+		})
+	);
 
-	let disposable = vscode.commands.registerCommand('canvas-dojo.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from canvas dojo!');
-	});
-
-	context.subscriptions.push(disposable);
-
-
-
-
-
-}
-
-
-class CanvasViewProvider implements vscode.WebviewViewProvider {
-
-	public static readonly viewType = 'calicoColors.colorsView';
-
-	private _view?: vscode.WebviewView;
-
-	constructor(
-		private readonly _extensionUri: vscode.Uri,
-	) { }
-
-	public resolveWebviewView(
-		webviewView: vscode.WebviewView,
-		context: vscode.WebviewViewResolveContext,
-		_token: vscode.CancellationToken,
-	) {
-		this._view = webviewView;
-
-		webviewView.webview.options = {
-			// Allow scripts in the webview
-			enableScripts: true,
-
-			localResourceRoots: [
-				this._extensionUri
-			]
-		};
-
-		webviewView.webview.html = this._getHtmlForWebview(webviewView.webview);
-
-		webviewView.webview.onDidReceiveMessage(data => {
-			switch (data.type) {
-				case 'colorSelected':
-					{
-						vscode.window.activeTextEditor?.insertSnippet(new vscode.SnippetString(`#${data.value}`));
-						break;
-					}
+	context.subscriptions.push(
+		vscode.commands.registerCommand('canvas-dojo.doRefactor', () => {
+			if (CanvasPanel.currentPanel) {
+				CanvasPanel.currentPanel.doRefactor();
 			}
-		});
-	}
-
-	private _getHtmlForWebview(webview: vscode.Webview) {
-		// Get the local path to main script run in the webview, then convert it to a uri we can use in the webview.
-		const scriptUri = webview.asWebviewUri(vscode.Uri.joinPath(this._extensionUri, 'media', 'main.js'));
-
-		// Do the same for the stylesheet.
-		const styleResetUri = webview.asWebviewUri(vscode.Uri.joinPath(this._extensionUri, 'media', 'reset.css'));
-		const styleVSCodeUri = webview.asWebviewUri(vscode.Uri.joinPath(this._extensionUri, 'media', 'vscode.css'));
-		const styleMainUri = webview.asWebviewUri(vscode.Uri.joinPath(this._extensionUri, 'media', 'main.css'));
-
-		// Use a nonce to only allow a specific script to be run.
-		const nonce = getNonce();
-
-		return `<!DOCTYPE html>
-			<html lang="en">
-			<head>
-				<meta charset="UTF-8">
-				<!--
-					Use a content security policy to only allow loading images from https or from our extension directory,
-					and only allow scripts that have a specific nonce.
-				-->
-				<meta http-equiv="Content-Security-Policy" content="default-src 'none'; style-src ${webview.cspSource}; script-src 'nonce-${nonce}';">
-				<meta name="viewport" content="width=device-width, initial-scale=1.0">
-				<link href="${styleResetUri}" rel="stylesheet">
-				<link href="${styleVSCodeUri}" rel="stylesheet">
-				<link href="${styleMainUri}" rel="stylesheet">
-				
-				<title>Cat Colors</title>
-			</head>
-			<body>
-        <canvas></canvas>
-				<script nonce="${nonce}" src="${scriptUri}"></script>
-			</body>
-			</html>`;
-	}
+		})
+	);
 }
+
+
+// class CanvasViewProvider implements vscode.WebviewViewProvider {
+
+// 	public static readonly viewType = 'canvas-dojo.canvasView';
+
+// 	private _view?: vscode.WebviewView;
+
+// 	constructor(
+// 		private readonly _extensionUri: vscode.Uri,
+// 	) { }
+
+// 	public resolveWebviewView(
+// 		webviewView: vscode.WebviewView,
+// 		context: vscode.WebviewViewResolveContext,
+// 		_token: vscode.CancellationToken,
+// 	) {
+// 		this._view = webviewView;
+
+// 		webviewView.webview.options = {
+// 			// Allow scripts in the webview
+// 			enableScripts: true,
+
+// 			localResourceRoots: [
+// 				this._extensionUri
+// 			]
+// 		};
+
+// 		webviewView.webview.html = this._getHtmlForWebview(webviewView.webview);
+
+// 		webviewView.webview.onDidReceiveMessage(data => {
+// 			switch (data.type) {
+// 				case 'colorSelected':
+// 					{
+// 						vscode.window.activeTextEditor?.insertSnippet(new vscode.SnippetString(`#${data.value}`));
+// 						break;
+// 					}
+// 			}
+// 		});
+// 	}
+
+//   public show() {
+//     if (this._view) {
+// 			this._view.show?.(true); // `show` is not implemented in 1.49 but is for 1.50 insiders
+// 			this._view.webview.postMessage({ type: 'show' });
+// 		}
+//   }
+
+// 	private _getHtmlForWebview(webview: vscode.Webview) {
+// 		// Get the local path to main script run in the webview, then convert it to a uri we can use in the webview.
+// 		// const scriptUri = webview.asWebviewUri(vscode.Uri.joinPath(this._extensionUri, 'media', 'main.js'));
+
+//     const editor = vscode.window.activeTextEditor;
+//     let scriptCode = "";
+//     if (editor) {
+//       const document = editor.document;
+//       scriptCode = document.getText();
+//     }
+
+// 		// Do the same for the stylesheet.
+// 		const styleResetUri = webview.asWebviewUri(vscode.Uri.joinPath(this._extensionUri, 'media', 'reset.css'));
+// 		const styleVSCodeUri = webview.asWebviewUri(vscode.Uri.joinPath(this._extensionUri, 'media', 'vscode.css'));
+// 		const styleMainUri = webview.asWebviewUri(vscode.Uri.joinPath(this._extensionUri, 'media', 'main.css'));
+
+// 		// Use a nonce to only allow a specific script to be run.
+// 		const nonce = getNonce();
+
+// 		return `<!DOCTYPE html>
+// 			<html lang="en">
+// 			<head>
+// 				<meta charset="UTF-8">
+// 				<!--
+// 					Use a content security policy to only allow loading images from https or from our extension directory,
+// 					and only allow scripts that have a specific nonce.
+// 				-->
+// 				<meta http-equiv="Content-Security-Policy" content="default-src 'none'; style-src ${webview.cspSource}; script-src 'nonce-${nonce}';">
+// 				<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+// 				<link href="${styleResetUri}" rel="stylesheet">
+// 				<link href="${styleVSCodeUri}" rel="stylesheet">
+// 				<link href="${styleMainUri}" rel="stylesheet">
+				
+// 				<title>Cat Colors</title>
+// 			</head>
+// 			<body>
+//         <canvas></canvas>
+// 				<script nonce="${nonce}">
+//         ${scriptCode}
+//         </script>
+// 			</body>
+// 			</html>`;
+// 	}
+// }
 
 function getNonce() {
 	let text = '';
